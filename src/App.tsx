72fcc9d1596a6e0c2e199202c17633409fd00c44
Helder S Ribeiro <helder@agentofuser.com>
2023-12-24 13:57:30 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -33,3 +33,12 @@ function App() {
 }
 
 export default App
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest
+  it('true', () => {
+    expect(0).toBe(0)
+    expect(1).toBe(1)
+    expect(6).toBe(6)
+  })
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
+// import { configDefaults } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    // ...configDefaults,
+    // testMatch: ['**/*.test.tsx'],
+    includeSource: ['src/**/*.{ts,tsx}'],
+    typecheck: {
+      enabled: true,
+    },
+  },
+  define: {
+    'import.meta.vitest': 'undefined',
+  },
 })
